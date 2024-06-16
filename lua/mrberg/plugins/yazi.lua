b26b85f88d3c17e81e3d048e8cfb793df516f5c3
Matt Berg <matt.berg@rechargeapps.com>
2024-06-15 21:42:01 -0700

    replace neotree and oil with yazi

---
+++ lua/mrberg/plugins/yazi.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    'mikavilpas/yazi.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    event = 'VeryLazy',
+    config = function()
+      require('yazi').setup()
+    end,
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        '<leader>e',
+        function()
+          require('yazi').yazi()
+        end,
+        desc = 'Open the file manager',
+      },
+      {
+        -- Open in the current working directory
+        '<leader>-',
+        function()
+          require('yazi').yazi(nil, vim.fn.getcwd())
+        end,
+        desc = "Open the file manager in nvim's working directory",
+      },
+    },
+    opts = {
+      open_for_directories = true,
+    },
+  },
+}
