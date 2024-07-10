078f87e47ee3198fe68a4badc17f708fab661bb9
Matt Berg <matt.berg95@gmail.com>
2024-07-09 21:02:12 -0700

    set conceallevel for md

--- lua/mrberg/core/options.lua
+++ lua/mrberg/core/options.lua
@@ -79,9 +79,11 @@ vim.opt.hidden = true
 
 -- Folds
 vim.o.foldcolumn = '1' -- '0' is not bad
-vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+vim.o.conceallevel = 1
 
 -- Prevent LSP from overwriting treesitter color settings
 -- https://github.com/NvChad/NvChad/issues/1907
