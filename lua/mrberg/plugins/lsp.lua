a1277e5a9724a544bbcdeeef897233c24637acd8
Matt Berg <matt.berg@rechargeapps.com>
2024-12-23 15:04:48 -0800

    remove lazydev

--- lua/mrberg/plugins/lsp.lua
+++ lua/mrberg/plugins/lsp.lua
@@ -11,20 +11,6 @@ return {
       { 'saghen/blink.cmp' },
       -- Useful status updates for LSP.
       { 'j-hui/fidget.nvim', opts = {} },
-      -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-      -- used for completion, annotations and signatures of Neovim apis
-      {
-        'folke/lazydev.nvim',
-        ft = 'lua', -- only load on lua files
-        opts = {
-          library = {
-            -- See the configuration section for more details
-            -- Load luvit types when the `vim.uv` word is found
-            { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-          },
-        },
-      },
-      { 'Bilal2453/luvit-meta', lazy = true },
     },
     opts = {
       servers = {
