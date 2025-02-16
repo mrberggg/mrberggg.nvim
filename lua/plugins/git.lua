106422048348d6052f37e96e5638848c1edba0a3
Matt Berg <matt.berg@rechargeapps.com>
2025-02-15 17:33:33 -0800

    git changes

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -9,14 +9,15 @@ return {
       require("vgit").setup()
     end,
     keys = {
-      { 'gj',          '<CMD>VGit hunk_down<CR>',                       desc = 'Previous hunk' },
-      { 'gk',          '<CMD>VGit hunk_up<CR>',                         desc = 'Next hunk' },
-      { '<leader>ghd', '<CMD>VGit buffer_hunk_preview<CR>',             desc = 'Git diff hunk' },
-      { '<leader>ghs', '<CMD>VGit buffer_hunk_stage<CR>',               desc = 'Git stage hunk' },
-      { '<leader>gh!', '<CMD>VGit buffer_hunk_reset<CR>',               desc = 'Git discard hunk' },
+      { ']g',          '<CMD>VGit hunk_down<CR>',                       desc = 'Previous hunk' },
+      { '[g',          '<CMD>VGit hunk_up<CR>',                         desc = 'Next hunk' },
+      { '<leader>gp',  '<CMD>VGit buffer_hunk_preview<CR>',             desc = 'Git diff hunk' },
+      { '<leader>gS',  '<CMD>VGit buffer_hunk_stage<CR>',               desc = 'Git stage hunk' },
+      { '<leader>g!',  '<CMD>VGit buffer_hunk_reset<CR>',               desc = 'Git discard hunk' },
       { '<leader>gcb', '<CMD>VGit buffer_conflict_accept_both<CR>',     desc = 'Git conflict accept both' },
       { '<leader>gcc', '<CMD>VGit buffer_conflict_accept_current<CR>',  desc = 'Git conflict accept current' },
       { '<leader>gci', '<CMD>VGit buffer_conflict_accept_incoming<CR>', desc = 'Git conflict accept incoming' },
+      { '<leader>gd',  '<CMD>VGit project_diff_preview<CR>',            desc = 'Git conflict accept incoming' },
     }
   }
 }
