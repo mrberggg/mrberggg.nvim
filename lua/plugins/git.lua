5afb02eff003bdb899bb32b570d31240dee76db9
Matt Berg <matt.berg@rechargeapps.com>
2025-02-27 10:19:16 -0800

    update git keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,56 +5,47 @@ return {
     config = function()
       require('gitsigns').setup {
         current_line_blame = true,
-        on_attach = function(bufnr)
-          local gitsigns = require('gitsigns')
-
-          local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-          end
-
-          -- Navigation
-          map('n', ']]', function()
-            if vim.wo.diff then
-              vim.cmd.normal({ ']]', bang = true })
-            else
-              gitsigns.nav_hunk('next')
-            end
-          end)
-
-          map('n', '[[', function()
-            if vim.wo.diff then
-              vim.cmd.normal({ '[[', bang = true })
-            else
-              gitsigns.nav_hunk('prev')
-            end
-          end)
-
-          -- Diffs
-          map('n', '<leader>gd', gitsigns.diffthis)
-          map('n', '<leader>gw', gitsigns.toggle_word_diff)
-          map('n', '<leader>g-', gitsigns.toggle_deleted)
-          map('n', '<leader>gp', gitsigns.preview_hunk)
-
-          -- Manage hunks
-          map('n', '<leader>gs', gitsigns.stage_hunk)
-          map('n', '<leader>gr', gitsigns.reset_hunk)
-          map('n', '<leader>gS', gitsigns.stage_buffer)
-          map('n', '<leader>gR', gitsigns.reset_buffer)
-
-          -- Add changed files to quick fix list
-          map('n', '<leader>gq', function() gitsigns.setqflist('all') end)
-
-          -- Git blame
-          map('n', '<leader>gb', function()
-            gitsigns.blame_line({ full = true })
-          end)
-          map('n', '<leader>gB', function()
-            gitsigns.blame()
-          end)
-        end
       }
     end,
-  }
+    keys = {
+      -- Nav
+      {
+        ']]',
+        function()
+          if vim.wo.diff then
+            vim.cmd.normal({ ']]', bang = true })
+          else
+            require('gitsigns').nav_hunk('next')
+          end
+        end,
+        desc = 'Git next hunk'
+      },
+      {
+        '[[',
+        function()
+          if vim.wo.diff then
+            vim.cmd.normal({ '[[', bang = true })
+          else
+            require('gitsigns').nav_hunk('prev')
+          end
+        end,
+        desc = 'Git next hunk'
+      },
+      -- Diffs
+      { '<leader>gd', '<CMD>Gitsigns diffthis<CR>',                                   desc = 'Git diff this' },
+      { '<leader>gw', '<CMD>Gitsigns toggle_word_diff<CR>',                           desc = 'Git toggle word diff' },
+      { '<leader>g-', '<CMD>Gitsigns toggle_deleted<CR>',                             desc = 'Git toggle deleted' },
+      { '<leader>gp', '<CMD>Gitsigns preview_hunk<CR>',                               desc = 'Git preview hunk' },
+      -- Manage hunks
+      { '<leader>gs', '<CMD>Gitsigns stage_hunk<CR>',                                 desc = 'Git stage hunk' },
+      { '<leader>gr', '<CMD>Gitsigns reset_hunk<CR>',                                 desc = 'Git reset hunk' },
+      { '<leader>gS', '<CMD>Gitsigns stage_buffer<CR>',                               desc = 'Git stage buffer' },
+      { '<leader>gR', '<CMD>Gitsigns reset_buffer<CR>',                               desc = 'Git reset buffer' },
+      -- Add changed files to quick fix list
+      { '<leader>gq', function() require('gitsigns').setqflist('all') end,            desc = 'Add all modified files to QF list' },
+      -- Git blame
+      { '<leader>gb', function() require('gitsigns').blame_line({ full = true }) end, desc = 'Git blame line' },
+      { '<leader>gB', function() require('gitsigns').blame() end,                     desc = 'Git blame buffer' },
+    }
+  },
 }
