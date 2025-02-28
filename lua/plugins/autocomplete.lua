d2677b97d30dcf552219f42a778e0e25c68ede8a
Matt Berg <matt.berg@rechargeapps.com>
2025-02-27 16:31:32 -0800

    fix cmdline autocomplete

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -6,13 +6,13 @@ return {
       -- skip autopair when next character is one of these
       skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
       -- skip autopair when the cursor is inside these treesitter nodes
-      skip_ts = { "string" },
+      skip_ts = { 'string' },
       -- skip autopair when next character is closing pair
       -- and there are more closing pairs than opening pairs
       skip_unbalanced = true,
       -- better deal with markdown code blocks
       markdown = true,
-    }
+    },
   },
   {
     'windwp/nvim-ts-autotag',
@@ -26,7 +26,17 @@ return {
     cond = not vim.g.vscode,
     version = '*',
     opts = {
-      cmdline = { completion = { ghost_text = { enabled = false } } },
+      cmdline = {
+        keymap = {
+          preset = 'enter',
+          ['<C-j>'] = { 'select_next', 'fallback' },
+          ['<C-k>'] = { 'select_prev', 'fallback' },
+        },
+        completion = {
+          ghost_text = { enabled = false },
+          menu = { auto_show = true }
+        },
+      },
       completion = {
         list = {
           selection = {
