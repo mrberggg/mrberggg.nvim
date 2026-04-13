1a540bfbdefbf30f672a89e597f7fb0e58580468
Matt Berg <matt.berg@rechargeapps.com>
2026-04-13 10:02:48 -0700

    use ts-forge

--- init.lua
+++ init.lua
@@ -5,17 +5,6 @@ vim.loader.enable()
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Build hooks -- must be defined before any vim.pack.add() calls
-vim.api.nvim_create_autocmd('PackChanged', {
-  callback = function(ev)
-    local name, kind = ev.data.spec.name, ev.data.kind
-    if name == 'nvim-treesitter' and (kind == 'install' or kind == 'update') then
-      if not ev.data.active then vim.cmd.packadd('nvim-treesitter') end
-      vim.cmd('TSUpdate')
-    end
-  end,
-})
-
 -- [[ Include config files ]]
 require 'core.options'
 
--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,24 +1,63 @@
 if vim.g.vscode then return end
 
 vim.pack.add {
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'master' },
+  'https://github.com/reybits/ts-forge.nvim',
 }
 
----@diagnostic disable-next-line: missing-fields
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'javascript', 'json', 'tsx', 'regex', 'vue' },
+-- Neovim 0.12 bundles parsers for: c, lua, markdown, markdown_inline, query, vim, vimdoc
+-- ts-forge installs additional parsers async on startup.
+require('ts-forge').setup {
   auto_install = true,
-  highlight = {
-    enable = true,
-  },
-  indent = { enable = true },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = '-',
-      node_incremental = '-',
-      scope_incremental = false,
-      node_decremental = '_',
-    },
+  ensure_installed = {
+    'typescript', 'tsx', 'javascript', 'json',
+    'html', 'css', 'bash', 'yaml', 'python',
   },
 }
+
+vim.treesitter.language.register('tsx', { 'typescriptreact' })
+vim.treesitter.language.register('json', { 'jsonc' })
+
+-- Use Vim's built-in syntax highlighting; treesitter is used only for
+-- indent, folds, and incremental selection (parser attaches lazily).
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(ev)
+    if pcall(vim.treesitter.get_parser, ev.buf) then
+      vim.bo[ev.buf].indentexpr = "v:lua.require'vim.treesitter'.indentexpr()"
+    end
+  end,
+})
+
+-- Incremental selection via treesitter nodes
+local sel_node = nil
+
+vim.keymap.set('n', '-', function()
+  sel_node = vim.treesitter.get_node()
+  if not sel_node then return end
+  local sr, sc, er, ec = sel_node:range()
+  vim.fn.setpos("'<", { 0, sr + 1, sc + 1, 0 })
+  vim.fn.setpos("'>", { 0, er + 1, ec, 0 })
+  vim.cmd('normal! gv')
+end, { desc = 'Init treesitter selection' })
+
+vim.keymap.set('v', '-', function()
+  if sel_node and sel_node:parent() then
+    sel_node = sel_node:parent()
+    local sr, sc, er, ec = sel_node:range()
+    vim.fn.setpos("'<", { 0, sr + 1, sc + 1, 0 })
+    vim.fn.setpos("'>", { 0, er + 1, ec, 0 })
+    vim.cmd('normal! gv')
+  end
+end, { desc = 'Expand treesitter selection' })
+
+vim.keymap.set('v', '_', function()
+  if sel_node then
+    local child = sel_node:named_child(0)
+    if child then
+      sel_node = child
+    end
+    local sr, sc, er, ec = sel_node:range()
+    vim.fn.setpos("'<", { 0, sr + 1, sc + 1, 0 })
+    vim.fn.setpos("'>", { 0, er + 1, ec, 0 })
+    vim.cmd('normal! gv')
+  end
+end, { desc = 'Shrink treesitter selection' })
