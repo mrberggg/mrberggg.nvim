9f15630845c0356aeecd860c999fc125e5ab638c
Matt Berg <matt.berg@rechargeapps.com>
2026-06-29 17:40:12 -0700

    update gtd

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -20,7 +20,7 @@ vim.keymap.set('n', '_', '<C-w>s', { desc = 'Split window horizontally' })
 
 vim.keymap.set('n', '<leader>i', '<Cmd>echo fnamemodify(expand("%"), ":~:.")<CR>', { desc = 'Present directory' })
 
-vim.keymap.set('n', 'gd', '<C-]>', { desc = '[G]o to [d]efinition' })
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = '[G]o to [d]efinition' })
 vim.keymap.set('n', '<leader>.', ':lua vim.lsp.buf.code_action()<CR>', { desc = '[C]ode [A]ction' })
 
 vim.keymap.set('n', 'r', "'", { desc = '[C]ode [A]ction' })
